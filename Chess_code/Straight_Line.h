af3648a3d9e4ae67f283c5be993f9abd4f61df2b
Aaron-Glave <aaronglavenowbelivesingod@gmail.com>
2024-09-19 10:06:40 -0400

    Update and rename Straight_Line.h to Chess_code/Straight_Line.h

---
+++ Chess_code/Straight_Line.h
@@ -0,0 +1,4 @@
+#include "Board.h"
+#pragma once
+
+bool can_move_stratight(Piece* piecetomove, int b_row, int b_column, Board* board_to_move_on);
--- Straight_Line.h
+++
@@ -1,4 +0,0 @@
-#include "Board.h"
-#pragma once
-
-bool can_move_stratight(Piece* piecetomove, int b_row, int b_column, Board* board_to_move_on);
