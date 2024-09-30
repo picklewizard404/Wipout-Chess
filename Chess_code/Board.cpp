e2f1e7218d0914ee02819391b739f5a64074d0c5
Aaron-Glave <aaronglavenowbelivesingod@gmail.com>
2024-09-30 16:30:43 -0400

    Test upgading Board.cpp

--- Chess_code/Board.cpp
+++ Chess_code/Board.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include "Safety.h"
+#include "Check_vs_Checkmate.h"
+#include "Move.h"
 using namespace std;
 
 Board::Board() {
@@ -38,32 +40,106 @@ bool Board::can_fight(COLOR my_team, int row, int column) {
     }
 }
 
-bool Board::is_in_check(King *king_of_team_1, Team *team_2, Board *mainboard) {
-    int column = king_of_team_1->column;
-    int row = king_of_team_1->row;
+//TODO pass both teams. Check where this function is called and make sure teams are passed.
+//Maybe I am calling this function for the wrong team?
+//The team we are checking check for is team 1.
+Game_Status Board::is_in_check(Team* my_team, Team* enemy_team, Board* mainboard, bool check_for_checkmate)  {
+    //Todo: Use info HERE to try every possible move of my_team
+    int column = my_team->the_king.column;
+    int row = my_team->the_king.row;
+    bool is_hugging_allowed = false;
     for (int i = 0; i < 16; i++) {
-        if (team_2->pieces[i] != 0) {
-            switch (team_2->pieces[i]->piecetype) {
+        if (enemy_team->pieces[i] != 0) {
+            switch (enemy_team->pieces[i]->piecetype) {
             case KING:
-                if ((King*)team_2->pieces[i]->can_classmove(row, column, mainboard)) {
-                    return true;
+                if ((King*)enemy_team->pieces[i]->can_classmove(row, column, mainboard)) {
+                    if (check_for_checkmate) {
+                        return try_to_escape(my_team, enemy_team, mainboard);
+                    }
+                    else return CHECK;
                 }
             case KNIGHT:
-                if ((Knight*)team_2->pieces[i]->can_classmove(row, column, this)) {
-                    return true;
+                if ((Knight*)enemy_team->pieces[i]->can_classmove(row, column, this)) {
+                    if (check_for_checkmate) {
+                        return try_to_escape(my_team, enemy_team, mainboard);
+                    }
+                    else return CHECK;
                 }
             case ROOK:
-                if ((Rook*)team_2->pieces[i]->can_classmove(row, column, this)) {
-                    return true;
+                if ((Rook*)enemy_team->pieces[i]->can_classmove(row, column, this)) {
+                    if (check_for_checkmate) {
+                        return try_to_escape(my_team, enemy_team, mainboard);
+                    }
+                    else return CHECK;
                 }
-            //Empty spaces go here and do not threaten you
+                //Empty spaces go here and do not threaten you
             default:
                 continue;
             }
         }
     }
-    return false;
+    return NEUTRAL;
 }
+
+
+Game_Status Board::try_to_escape(Team* my_team, Team* enemy_team, Board* mainboard) {
+    Piece* one_of_my_pieces;
+    Move tried_move;
+    Game_Status current_status = CHECK;
+    //I don't really care about hugging so I will assume I didn't land on a king;
+    bool landed_on_a_king = false;
+    for (int i = 0; i < 16; i++) {
+        one_of_my_pieces = my_team->pieces[i];
+        if (my_team->pieces[i] == NULL) {
+            continue;
+        }
+        if (one_of_my_pieces->alive == false) {
+            continue;
+        }
+        tried_move.start_row = one_of_my_pieces->row;
+        tried_move.start_column = one_of_my_pieces->column;
+        tried_move.piece_that_moved = one_of_my_pieces;
+        for (int row = 1; row <= 8; row++) {
+            for (int column = 1; column <= 8; column++) {
+                if (one_of_my_pieces->can_classmove(row, column, mainboard)) {
+                    tried_move.end_row = row;
+                    tried_move.end_column = column;
+                    tried_move.piece_landed_on = mainboard->spaces[row - 1][column - 1];
+                    mainboard->move_piece(&tried_move);
+                    //TODO: Check if you are still in check. If you are, the loop continues.
+                    //If not, return CHECK because we know we can be safe again.
+                    //Either way the move must be un_done.
+                    if (mainboard->is_in_check(my_team, enemy_team, mainboard, false) == NEUTRAL) {
+                        //TODO: Undo the move.
+                        Move undomove = Move(
+                            tried_move.end_row,
+                            tried_move.end_column,
+                            tried_move.start_row,
+                            tried_move.start_column,
+                            tried_move.piece_that_moved,
+                            NULL
+                        );
+                        mainboard->move_piece(&undomove);
+                        one_of_my_pieces->row = tried_move.start_row;
+                        one_of_my_pieces->column = tried_move.start_column;
+                        if (tried_move.piece_landed_on != NULL) {
+                            tried_move.piece_landed_on->alive = true;
+                            mainboard->place(tried_move.piece_landed_on, row, column);
+                        }
+                        return CHECK;
+                    }
+                    else {
+                        //TODO Undo the move
+                    }
+                }
+            }
+        }
+    }
+    //If this loop ends, you can't escape Check.
+    return CHECKMATE;
+
+};
+
 bool Board::is_on_board(int b_row, int b_column) {
     if (b_row < 1 || b_column < 1 || b_row > 8 || b_column > 8) {
         return false;
@@ -75,28 +151,32 @@ b_column and b_row range from 1 to 8. We subtract 1 whenever we need to hit a sp
 That's because a person starts counting spaces with 1 but the computer starts counting with 0.
 Do this whenever you touch a space on the board.
 */
-bool Board::move(space piece, int b_row, int b_column, bool* landed_on_king) {
+bool Board::move_piece(Move* move_to_make) {
+    space piece = move_to_make->piece_that_moved;
+    int b_row = move_to_make->end_row;
+    int b_column = move_to_make->end_column;
     if (piece != NULL) {
         int p_column = piece->column;
         int p_row = piece->row;
-        bool can_move = false;
-        can_move = can_move || piece->can_classmove(b_row, b_column, this);
+        bool can_move = piece->can_classmove(b_row, b_column, this);
         if (piece != NULL) {
             if (piece->piecetype == KING) {
-                can_move = can_move || ((King*)piece)->can_kingmove(b_row, b_column, this, landed_on_king);
+                can_move = can_move || ((King*)piece)->can_kingmove(b_row, b_column, this);
             }
         }
         if (can_move) {
             // If you landed on a piece on your team:
             if (piece->do_team_match(spaces[b_row - 1][b_column - 1])) {
-                printf("You have a piece there, silly!\n");
                 return false;
             }
 
             //Opposing team:
             if (spaces[b_row - 1][b_column - 1] != NULL) {
                 spaces[b_row - 1][b_column - 1]->alive = false;
+                spaces[b_row - 1][b_column - 1] = piece;
                 //Remember the fact that I was there in case this move has to be redone.
+                move_to_make->piece_landed_on = spaces[b_row - 1][b_column - 1];
+                //TODO: Add this to the stack of moves.
             }
 
 
